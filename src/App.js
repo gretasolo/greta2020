6e712e115a4cd83d1da31ee341555c6f4c47a235
Greta Solomon <gs2806@nyu.edu>
2020-05-11 02:14:34 -0500

    5

--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ function Main(props) {
 
 <div id= "titlestuff"> 
 
-
-<h1><strong>Interdisciplinary Analysis of Big Tech Companies in Cities</strong> </h1>
 <div className='main'>
-      <div id= "title"> <strong><p id="titleline"> By Greta Solomon </p> </strong></div>
+<h1><strong>Interdisciplinary Analysis of Big Tech Companies in Cities</strong> </h1>
+
+<div id="titlelines"><strong><p id="titleline"> By Greta Solomon </p> </strong></div>
 </div>
+
 
 <div className="firstline">
 
+
+      
       <button id="Intro" onClick>Intro</button>
       <button id="Litreview" onClick>Literature</button>
       <button id="Method" onClick>Method</button>
@@ -33,14 +36,40 @@ function Main(props) {
 <br></br>
 
 <div class="videos">
-<div id="video"><iframe class="iframe" src={mainContent[0]} iframe width="560" height="315" frameborder="0" allow="autoplay" allow="fullscreen" mute="true" loop="true"></iframe> </div>
-</div></div>
+<div id="video"><iframe iframe="true" src={mainContent[0]} iframe width="560" height="315" frameborder="0" allow="autoplay" allow="fullscreen" mute="true" loop="true"></iframe> </div>
+</div>
 
+<div className="sections">
+
+  <div id="intro">
+  <p>Introduction</p>
+ <img src= "https://i.ibb.co/xs8B5Rj/JMJ3998.jpg" height = "400" width = "300" />
+  </div>
+  
+  <div id="lit">
+  <p>Literature Review</p>
+  <img src= "https://i.ibb.co/ZSNMRpB/JMJ4025.jpg" height = "300" width = "500" />
+  </div>
+  <div id="method">
+  <p>Method</p>
+  <img src= "https://i.ibb.co/dLcpvLM/JMJ4021.jpg" height = "300" width = "500" />
+  </div>
+
+  <div id="analysis">
+  <p>Analysis</p>
+  <img src= "https://i.ibb.co/BPrgF0B/JMJ4017.jpg" height = "300" width = "500" />
+  </div>
+
+  <div id="future">
+  <p>Future Plans</p>
+  <img src= "https://i.ibb.co/YhfwP7P/JMJ4029.jpg" height = "300" width = "500" />
+  </div>
+
+</div></div>
 
   )
 
+
 }
-
-
 
 export default App;
